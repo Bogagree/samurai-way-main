4036df8d2b095ec56273a9de94795d238d297de5
Bogagree <bogatireshka@gmail.com>
2022-10-09 20:40:30 +0300

    refactor: BrowserRouter => HashRouter for gitHub

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./redux/redux-store";
 
-
-// export let renderEntireTree = () => {
     ReactDOM.render(
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <App/>,
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
         , document.getElementById('root')
     )
-
-// renderEntireTree()
-
-// store.subscribe(() => renderEntireTree())
